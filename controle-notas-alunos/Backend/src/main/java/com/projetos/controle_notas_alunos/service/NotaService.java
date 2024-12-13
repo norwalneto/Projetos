f57f8bff4c20fd20fed4a56f1d188ba9468392a1
norwalneto <norwalgomess@gmail.com>
2024-12-12 22:03:49 -0300

    implementação do NotaService.

--- controle-notas-alunos/Backend/src/main/java/com/projetos/controle_notas_alunos/service/NotaService.java
+++ controle-notas-alunos/Backend/src/main/java/com/projetos/controle_notas_alunos/service/NotaService.java
@@ -1,8 +1,28 @@
 package com.projetos.controle_notas_alunos.service;
 
+import com.projetos.controle_notas_alunos.model.Nota;
+import com.projetos.controle_notas_alunos.repository.NotaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class NotaService {
+
+    private final NotaRepository notaRepository;
+
+    @Autowired
+    public NotaService( NotaRepository notaRepository){
+        this.notaRepository = notaRepository;
+    }
+
+    public Nota salvar(Nota nota){
+        return notaRepository.save(nota);
+    }
+
+    public List<Nota> listarPorAluno(Long alunoId){
+        return notaRepository.findByAlunoId(alunoId);
+    }
     
 }
